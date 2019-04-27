0dd99b0223529eb302cafeb42335b988b67a20a6
iammadab <wisdom_ogwu@yahoo.com>
2019-04-27 10:02:45 +0100

    Added more robust errors for fetch

--- resources/shipment/shipment_fetch.js
+++ resources/shipment/shipment_fetch.js
@@ -1,3 +1,4 @@
+const lazyError = require("lazy-error")
 const Shipment = require("./shipment_model")
 const { createValidator } = require("../../lib/validator")
 
@@ -6,6 +7,20 @@ const fetchShipmentValidator = createValidator("shipmentId.string")
 function shipmentFetchFn({ shipmentId }){
 	return Shipment.find({ _id: shipmentId })
 				   .populate("parcels")
+				   .then(checkForParcels)
+				   .catch(handleErrors)
+
+	function checkForParcels(parcels){
+		if(parcels.length == 0)
+			throw lazyError.createError(404, "SHIPMENT_NOT_FOUND")
+		return parcels
+	}
+
+	function handleErrors(err){
+		if(err.name == "CastError")
+			throw lazyError.createError(400, "INVALID_ID")
+		throw err
+	}
 }
 
 function shipmentFetchRoute(req, res){
@@ -15,6 +30,7 @@ function shipmentFetchRoute(req, res){
 		.then(() => shipmentFetchFn(req.params))
 		.then(createSuccessResponse)
 		.then(response => res.status(200).json(response))
+		.catch(res.sendError)
 
 	function sendBadRequestError(errors){
 		res.sendError(400, "BAD_REQUEST_BODY", errors)
